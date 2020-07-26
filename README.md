d76731345b9f623e897bd31cc60d2782dcdcae28
Bruno Teixeira <bruno_teixeira@hotmail.com>
2020-07-26 18:04:33 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto PizzaDev
+- API do meu projeto de Pizzaria
+- tecnologias usadas ( **NodeJs** e **ReactJs** )
---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+require('dotenv').config({path: 'variables.env'})
+
+const app = express()
+
