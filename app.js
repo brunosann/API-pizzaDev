0a93efc784a7f858d68d40c67ccd46a610ee6a99
Bruno Teixeira <bruno_teixeira@hotmail.com>
2020-08-19 14:14:38 -0300

    start/cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("dotenv").config({ path: "variables.env" });
+const cors = require("cors");
 const router = require("./src/routes");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
